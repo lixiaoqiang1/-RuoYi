e6d4d73825115b9555eca4a2c6436f770e8e5cff
lixiaoqiang1 <361028458@qq.com>
2022-09-28 16:31:19 +0800

    111

---
+++ src/views/rolemanagement/addrole/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>111
+        <p>
+          <el-button
+            type="primary"
+            size="mini"
+            icon="el-icon-cloudy"
+            plain
+            @click="goTarget"
+            >访问码云</el-button
+          >
+        </p>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    },
+    created(){},
+    mounted(){},
+    methods:{
+        goTarget() {
+            this.$router.push({ path: 'rolelist' })
+        },
+    }
+}
+</script>
---
+++ src/views/rolemanagement/rolelist/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>222</div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    },
+    created(){},
+    mounted(){},
+    methods:{
+
+    }
+}
+</script>
